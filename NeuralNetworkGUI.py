bd7511b8f60dd9a538db66f305337d64a603498e
Cian Duffy <cianduffyty@gmail.com>
2016-11-18 12:25:00 +0000

    GUI Classify and reset functionality

--- NeuralNetworkGUI.py
+++ NeuralNetworkGUI.py
@@ -1,7 +1,10 @@
 import Tkinter
 from tkFileDialog import askopenfilename
+
 import ntpath
+
 import numpy as np
+
 from NeuralNetworkDriver import NeuralNetworkDriver
 from NeuralNetworkTester import NeuralNetworkTester
 
@@ -13,24 +16,23 @@ class NeuralNetworkGUI(object):
         self.main_window.wm_title("Artificial Neural Network")
         self.main_window.resizable(width=False, height=False)
         self.initial_width = 700
-        self.initial_height = 500
+        self.initial_height = 550
         self.main_window.geometry('{}x{}'.format(self.initial_width, self.initial_height))
 
-        # Initialise colours
-        self.gallery_colour = "#EFEFEF"
-        # Add canvas for instruction text
-        self.text_canvas = Tkinter.Canvas(self.main_window, width=self.initial_width, height=self.initial_height - 5)
-        self.centre_label = self.text_canvas.create_text(self.initial_width / 2, self.initial_height / 2, anchor="c",
-                                                         font="Helvetica", text='Import your dataset to get started')
-        self.text_canvas.place(relx=0.5, rely=-0.1, anchor="n")
-        self.text_canvas.configure(background=self.gallery_colour)
+        self.top_label_text = Tkinter.StringVar()
+        self.top_label = Tkinter.Label(self.main_window, textvariable=self.top_label_text, font=("Helvetica", 24))
+        self.top_label_text.set("Import your dataset to get started")
+        self.top_label.pack(pady=10, padx=20)
 
         self.import_button = Tkinter.Button(self.main_window, text="Import Dataset...",
                                             command=self.on_click_import_button)
-        self.import_button.place(relx=0.5, rely=0.6, anchor="c")
+        self.import_button.pack()
 
+        self.classification_details_container = Tkinter.Frame()
+        self.classification_details_label = Tkinter.Label()
         self.filename = ""
         self.filepath = ""
+        self.final_results = []
         self.hidden_layer_sizes = range(10, 51, 10)
         self.training_iterations = range(1000, 10001, 1000)
 
@@ -40,47 +42,39 @@ class NeuralNetworkGUI(object):
         opts = {'filetypes': [('CSV files', '.csv')]}
         self.filepath = askopenfilename(**opts)  # show an "Open" dialog box and return the path to the selected file
         self.filename = self.get_filename_from_path(self.filepath)
-        self.update_centre_label(self.filename)
+        if self.filename != "":
 
-        self.import_button.destroy()
-        float_width = float(self.initial_width)
-        float_height = float(self.initial_height)
+            self.top_label_text.set(self.filename + " successfully imported")
 
-        bottom_buttons_y_position = (float_height - 20.0) / float_height
-        reset_button_x_position = 20.0 / float_width
-        test_button_x_position = (float_width - 20.0) / float_width
+            self.import_button.destroy()
+            float_width = float(self.initial_width)
+            float_height = float(self.initial_height)
 
-        self.reset_button = Tkinter.Button(self.main_window, text="Reset", command=self.on_click_reset_button)
-        self.reset_button.place(relx=reset_button_x_position, rely=bottom_buttons_y_position, anchor="sw")
+            bottom_buttons_y_position = (float_height - 20.0) / float_height
+            reset_button_x_position = 20.0 / float_width
+            test_button_x_position = (float_width - 20.0) / float_width
 
-        self.classify_button = Tkinter.Button(self.main_window, text=("Classify " + self.filename),
-                                              command=self.on_click_classify_button)
-        self.classify_button.place(relx=0.5, rely=bottom_buttons_y_position, anchor="s")
+            self.reset_button = Tkinter.Button(self.main_window, text="Reset", command=self.on_click_reset_button)
+            self.reset_button.place(relx=reset_button_x_position, rely=bottom_buttons_y_position, anchor="sw")
 
-        self.test_button = Tkinter.Button(self.main_window, text=("Test " + self.filename),
-                                          command=self.on_click_test_button)
-        self.test_button.place(relx=test_button_x_position, rely=bottom_buttons_y_position, anchor="se")
+            self.classify_button = Tkinter.Button(self.main_window, text=("Classify " + self.filename),
+                                                  command=self.on_click_classify_button)
+            self.classify_button.place(relx=0.5, rely=bottom_buttons_y_position, anchor="s")
 
-        self.main_window.mainloop()
+            self.test_button = Tkinter.Button(self.main_window, text=("Test " + self.filename),
+                                              command=self.on_click_test_button)
+            self.test_button.place(relx=test_button_x_position, rely=bottom_buttons_y_position, anchor="se")
 
     def on_click_classify_button(self):
-        self.update_centre_label("Classifying")
-        driver = NeuralNetworkDriver(self.filepath)
-        final_results = driver.build_network_and_classify_data()
-        print final_results
+        self.reset_button.configure(state="disabled")
+        self.classify_button.configure(state="disabled")
+        self.test_button.configure(state="disabled")
+        self.top_label_text.set("Classifying " + self.filename + "...")
 
-        accuracy_sum = 0
-        index = 1
-        for result_dictionary in final_results:
-            self.print_classification_results(result_dictionary, index)
-            accuracy_sum += float(result_dictionary['accuracy'])
-
-        mean_accuracy = accuracy_sum / len(final_results)
-        self.update_centre_label("Classification complete.\nMean accuracy: " + str(round(100 * mean_accuracy, 2)) + "%")
-        self.main_window.mainloop()
+        self.main_window.after(500, self.begin_classification)
 
     def on_click_test_button(self):
-        self.update_centre_label("Testing")
+        self.top_label_text.set("Testing network attributes using " + self.filename)
         tester = NeuralNetworkTester(self.filepath)
         tester.test_network_and_plot_results()
 
@@ -88,53 +82,95 @@ class NeuralNetworkGUI(object):
         self.reset_button.destroy()
         self.classify_button.destroy()
         self.test_button.destroy()
+        self.classification_details_label.destroy()
+        self.classification_details_container.destroy()
 
         self.filename = ""
         self.filepath = ""
 
         self.import_button = Tkinter.Button(self.main_window, text="Import Dataset...",
                                             command=self.on_click_import_button)
-        self.import_button.place(relx=0.5, rely=0.6, anchor="c")
+        self.import_button.pack()
 
-        self.centre_label.configure(text="Import your dataset to get started")
-        self.main_window.mainloop()
+        self.top_label_text.set("Import your dataset to get started")
 
     @staticmethod
     def get_filename_from_path(filepath):
         return ntpath.basename(filepath)
 
-    def print_classification_results(self, result_dictionary, index):
-        input_array = result_dictionary['input']
-        result_array = result_dictionary['result']
-        accuracy = float(result_dictionary['accuracy'])
-        print "----------------------------------------------------------"
-        print "Iteration #" + str(index)
-        print "Input list:"
-        print input_array
-        print "Predicted list:"
-        print result_array
-        print "Matched Entries:"
-        print self.matched_result_list_for_input_lists(input_array, result_array)
-        print "Accuracy: " + str(round(100 * accuracy, 2)) + "%"
-
     @staticmethod
-    def matched_result_list_for_input_lists(input, result):
+    def matched_result_list_for_input_lists(target, result):
         matched_list = []
-        for index in range(0, len(input), 1):
-            if input[index] == result[index]:
-                matched_list.append("Match")
+        for index in range(0, len(target), 1):
+            if target[index] == result[index]:
+                matched_list.append("Correct")
             else:
-                matched_list.append("Fail")
+                matched_list.append("Incorrect")
 
         return np.array(matched_list)
 
-    def update_centre_label(self, label_text):
-        self.text_canvas.destroy()
-        self.text_canvas = Tkinter.Canvas(self.main_window, width=self.initial_width, height=self.initial_height - 5)
-        self.text_canvas.place(relx=0.5, rely=-0.1, anchor="n")
-        self.text_canvas.configure(background=self.gallery_colour)
+    def begin_classification(self):
+        driver = NeuralNetworkDriver(self.filepath)
+        self.final_results = driver.build_network_and_classify_data()
 
-        self.centre_label = self.text_canvas.create_text(self.initial_width / 2, self.initial_height / 2, anchor="c",
-                                                         font="Helvetica", text=label_text)
+        self.display_classification_results()
 
+        accuracy_sum = 0
+        for result_dictionary in self.final_results:
+            accuracy_sum += float(result_dictionary['accuracy'])
 
+        mean_accuracy = accuracy_sum / len(self.final_results)
+        self.reset_button.configure(state="normal")
+        self.classify_button.configure(state="normal")
+        self.test_button.configure(state="normal")
+        self.top_label_text.set("Classification complete!\nMean accuracy: " + str(round(100 * mean_accuracy, 2)) + "%")
+        self.main_window.mainloop()
+
+    def display_classification_results(self):
+        self.classification_details_label = Tkinter.Label(self.main_window, text="Classification Details:",
+                                                          font=("Helvetica", 16))
+        self.classification_details_label.pack(anchor="w", pady=10, padx=20)
+
+        self.classification_details_container = Tkinter.Frame(self.main_window, bd=1, relief="solid")
+
+        scrollbar = Tkinter.Scrollbar(self.classification_details_container)
+        scrollbar.pack(side="right", fill="y")
+
+        results_text_area = Tkinter.Text(self.classification_details_container, wrap="word", font="Helvetica")
+        results_text_area.pack(fill="x")
+
+        results_text_area.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=results_text_area.yview)
+        index = 1
+
+        for results_dictionary in self.final_results:
+            input_array = results_dictionary['input']
+            result_array = results_dictionary['result']
+            match_array = self.matched_result_list_for_input_lists(input_array, result_array)
+            accuracy = float(results_dictionary['accuracy'])
+            results_text_area.insert("end", "------------------------------------- Iteration #" + str(index)
+                                     + " -------------------------------------\n")
+            results_text_area.insert("end", "\nTarget Classes:\n")
+            results_text_area.insert("end", self.inline_string_for_array(input_array) + "\n")
+            results_text_area.insert("end", "\nPredicted Classes:\n")
+            results_text_area.insert("end", self.inline_string_for_array(result_array) + "\n")
+            results_text_area.insert("end", "\nCorrect Predictions:\n")
+            results_text_area.insert("end", self.inline_string_for_array(match_array) + "\n")
+            results_text_area.insert("end", "\nPrediction Accuracy: " + str(round(100 * accuracy, 2)) + "%\n\n")
+            index += 1
+
+        results_text_area.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=results_text_area.yview)
+
+        self.classification_details_container.pack(fill="x", padx=20)
+
+    @staticmethod
+    def inline_string_for_array(input_array):
+        inline_string = ""
+        index = 1
+        for entry in input_array:
+            inline_string += entry
+            if index < len(input_array):
+                inline_string += ", "
+            index += 1
+        return inline_string
